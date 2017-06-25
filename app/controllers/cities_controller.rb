71452ac6e3a827392c6b6cdd7d0e54c660e95d93
shiyongheng <ziyou@shiyonghengdeMacBook-Air.local>
2017-06-25 18:52:11 +0800

    可以更新城市天气

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,4 +2,16 @@ class CitiesController < ApplicationController
   def index
     @cities = City.all
   end
+
+  def update_temp
+    city = City.find(params[:id])
+
+    response = RestClient.get "http://v.juhe.cn/weather/index",
+                              :params => { :cityname => city.juhe_id, :key => "12858a06d553efcb157cb036fbb3ef01" }
+    data = JSON.parse(response.body)
+
+    city.update( :current_temp => data["result"]["sk"]["temp"] )
+
+    redirect_to cities_path
+end
 end
